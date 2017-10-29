e7d401317d04d6e1fa134113c296f4dedd566383
Tony Lu <tonyluj@gmail.com>
2017-10-30 02:13:00 +0800

    Bugfix: change status to uint8
    Signed-off-by: Tony Lu <tonyluj@gmail.com>

--- model/device.go
+++ model/device.go
@@ -5,12 +5,20 @@ import (
 	"github.com/pkg/errors"
 )
 
+type DeviceStatus uint8
+
+const (
+	DeviceStatusInit     DeviceStatus = 0
+	DeviceStatusBinded   DeviceStatus = 1
+	DeviceStatusUnbinded DeviceStatus = 2
+)
+
 type Device struct {
-	ID     int64  `json:"id" gorm:"primary_key,AUTO_INCREMENT"`
-	UserID int64  `json:"userId"`
-	Type   string `json:"type"`
-	UUID   string `json:"uuid"`
-	Status string `json:"status"`
+	ID     int64        `json:"id" gorm:"primary_key,AUTO_INCREMENT"`
+	UserID int64        `json:"userId"`
+	Type   string       `json:"type"`
+	UUID   string       `json:"uuid"`
+	Status DeviceStatus `json:"status"`
 
 	Timestamp
 }
@@ -28,6 +36,24 @@ func DevicesByUserID(db *gorm.DB, id int64) (devices []Device, err error) {
 	err = db.Where("id = ?", id).Find(&devices).Error
 	if err != nil {
 		err = errors.Wrap(err, "device by user id error")
+		return
+	}
+	return
+}
+
+func DeviceByUUID(db *gorm.DB, uuid string) (device Device, err error) {
+	err = db.Where("uuid = ?", uuid).First(&device).Error
+	if err != nil {
+		err = errors.Wrap(err, "device by uuid error")
+		return
+	}
+	return
+}
+
+func DeviceUpdateStatus(db *gorm.DB, device *Device) (err error) {
+	err = db.Table("device").Where("uuid = ?", device.UUID).Update("status", device.Status).Error
+	if err != nil {
+		err = errors.Wrap(err, "device status update error")
 		return
 	}
 	return
